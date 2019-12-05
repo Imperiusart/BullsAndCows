a4b79d9e220fc1c81a20ac8657fa2f9e82099964
Marcintyrakowski <jczarek89@gmail.com>
2019-12-05 01:11:06 +0100

    First code added.

---
+++ Section2/BullCowGame/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	constexpr int WORD_LENGHT = 5;
+	cout<< "Welcome to Bulls and Cows, a fun word game.\n" << endl;
+	cout << "can you guess the " <<WORD_LENGHT;
+	cout << " letter isogram I'm thinking of?\n";
+
+	return 0;
+}
